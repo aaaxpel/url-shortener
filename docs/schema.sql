2ab7e1c9bcbbc97eca1a39254f4654c94e467c60
aaaxpel <sqlc@mail.ru>
2026-04-16 20:03:25 +0700

    add db support and create end point

--- docs/schema.sql
+++ docs/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE "urls" (
   "id" integer PRIMARY KEY,
-  "short_code" varchar,
+  "short_code" varchar UNIQUE,
   "url" varchar,
-  "createdAt" timestamp,
-  "updatedAt" timestamp,
-  "accessCount" integer
+  "createdAt" timestamp DEFAULT CURRENT_TIMESTAMP,
+  "updatedAt" timestamp DEFAULT CURRENT_TIMESTAMP,
+  "accessCount" integer DEFAULT 0
 );
--- shorten.go
+++ shorten.go
@@ -1,16 +1,54 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/hex"
+	"encoding/json"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/aaaxpel/url-shortener/database"
 )
+
+var db = database.Connect()
+
+type Url struct {
+	ID          int
+	ShortCode   string
+	URL         string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	AccessCount int
+}
 
 func Create(w http.ResponseWriter, r *http.Request) {
 
-	// db := database.Connect()
-	r.PostFormValue("url")
+	length := 8
+	byteLength := (length + 1) / 2
+	randomBytes := make([]byte, byteLength)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		log.Fatal("boom")
+	}
+
+	shortCode := hex.EncodeToString(randomBytes)
+
+	query := "INSERT INTO urls (short_code, url) VALUES (?, ?) RETURNING id, short_code, url, createdAt, updatedAt"
+	newURL := Url{}
+	err = db.QueryRow(query, shortCode, r.PostFormValue("url")).Scan(&newURL.ID, &newURL.ShortCode, &newURL.URL, &newURL.CreatedAt, &newURL.UpdatedAt)
+	if err != nil {
+		log.Printf("error: %s", err)
+	}
 
 	w.WriteHeader(201)
-	w.Write([]byte("hi"))
+
+	new, err := json.Marshal(newURL)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	w.Write([]byte(new))
 }
 
 func Get(w http.ResponseWriter, r *http.Request) {
